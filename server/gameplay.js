3cb10a3a06a74614a87a1858defb3bdaee0b1a63
Christopher Langton <chrislangton84@gmail.com>
2015-01-24 15:43:07 +1100

    new stuff

--- server/gameplay.js
+++ server/gameplay.js
@@ -8,7 +8,11 @@ var dataset = {
       full_chest: 5,
       empty_chest: 6,
       key: 7,
-      door: 8
+      door: 8,
+      fire: 9,
+      water: 10,
+      bucket: 11,
+      extinguisher: 12
   };
 
 var PLAYER_COUNT = 4;
@@ -19,32 +23,32 @@ function nextTurn(gamestate){
 
   var playersNext = [];
 
-	for(var i=0; i<PLAYER_COUNT; i++){
-		console.log('processing player ' + i);
-		if(actions[i]){
-			var actionParsed = /([^_1-9]+)_?(\d+)?_(\d+)?/.exec(actions[i]);
-			console.log(actionParsed);
-			if(actionParsed[1] == 'move' && map[actionParsed[3]][actionParsed[2]] == dataset.empty){
-				gamestate.players[i].h = parseInt(actionParsed[2]);
-				gamestate.players[i].v = parseInt(actionParsed[3]);
-				console.log('player ' + i + ' moved');
-			}
-		}
-	}
+  for(var i=0; i<PLAYER_COUNT; i++){
+    console.log('processing player ' + i);
+    if(actions[i]){
+      var actionParsed = /([^_1-9]+)_?(\d+)?_(\d+)?/.exec(actions[i]);
+      console.log(actionParsed);
+      if(actionParsed[1] == 'move' && map[actionParsed[3]][actionParsed[2]] == dataset.empty){
+        gamestate.players[i].h = parseInt(actionParsed[2]);
+        gamestate.players[i].v = parseInt(actionParsed[3]);
+        console.log('player ' + i + ' moved');
+      }
+    }
+  }
 
   gamestate.pendingActions = {};
   gamestate.instance.turn++;
 }
 
 exports.create = function (id){
-	var gamestate = maps.cloneLevel('testLevel');
-	gamestate.dataset = dataset;
-	gamestate.instance = {
-		id: id,
-		turn: 0
-	};
-	gamestate.profile = 0;
-	gamestate.pendingActions = {};
+  var gamestate = maps.cloneLevel('testLevel');
+  gamestate.dataset = dataset;
+  gamestate.instance = {
+    id: id,
+    turn: 0
+  };
+  gamestate.profile = 0;
+  gamestate.pendingActions = {};
 
   return gamestate;
 }
@@ -53,8 +57,8 @@ exports.submitAction = function(gameState, playerId, action){
   console.log("submitted action " + action);
   gameState.pendingActions[playerId] = action;
 
-	if(Object.keys(gameState.pendingActions).length >= PLAYER_COUNT){
-		nextTurn(gameState);
-	}
-	return gameState;
+  if(Object.keys(gameState.pendingActions).length >= PLAYER_COUNT){
+    nextTurn(gameState);
+  }
+  return gameState;
 }
--- server/maps.js
+++ server/maps.js
@@ -1,41 +1,41 @@
 var levels = {
 
-	testLevel: {
-		map: [
-	      [0,0,0,0,5,0,0,0,0,0],
-	      [5,0,0,0,8,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,5,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0],
-	      [0,0,0,0,0,0,0,0,0,0]
-	    ],
-	    players: [
-	    	{
-	    		sprite: '1',
-	    		h: 1,
-	    		v: 0,
-	    		inventory: []
-	    	},
-	    	{
-	    		sprite: '2',
-	    		h: 3,
-	    		v: 2,
-	    		inventory: [7]
-	    	},
-	    	{
-	    		sprite: '3',
-	    		h: 4,
-	    		v: 4,
-	    		inventory: []
-	    	},
-	    	{
-	    		sprite: '4',
-	    		h: 5,
-	    		v: 5,
-	    		inventory: []
-	    	}
-	    ]
-	}
+  testLevel: {
+    map: [
+        [0,0,0,0,5,0,0,0,0,0],
+        [5,0,0,0,0,0,0,0,10,0],
+        [0,0,0,0,0,0,0,0,10,9],
+        [0,0,0,0,0,0,5,0,10,0],
+        [0,0,0,0,0,0,0,0,10,8],
+        [0,0,0,0,0,0,0,0,10,0]
+      ],
+      players: [
+        {
+          sprite: '1',
+          h: 1,
+          v: 0,
+          inventory: [12]
+        },
+        {
+          sprite: '2',
+          h: 3,
+          v: 2,
+          inventory: [7,11,7,7,7]
+        },
+        {
+          sprite: '3',
+          h: 4,
+          v: 4,
+          inventory: [12]
+        },
+        {
+          sprite: '4',
+          h: 5,
+          v: 5,
+          inventory: []
+        }
+      ]
+  }
 
 }
 
@@ -44,5 +44,5 @@ function clone(a) {
 }
 
 exports.cloneLevel = function(mapName){
-	return clone(levels[mapName]);
+  return clone(levels[mapName]);
 }
