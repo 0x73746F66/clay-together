7707ed7e0893d1a4ef5a60ccd3b39be4596f6890
Christopher Langton <chrislangton84@gmail.com>
2015-01-27 17:30:48 +1100

    0.0.4-dev

--- droptime/server.js
+++ droptime/server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app        = express();
 var router     = express.Router();
 var port       = process.env.PORT || 3001; // set our port
-var version    = '0.0.4-dev';
+var version    = '0.0.4';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
